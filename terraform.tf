0b9a3992a84226cd677f57c81908acf1bf4a7e97
M4t7e <7535523+M4t7e@users.noreply.github.com>
2024-10-26 10:38:08 +0200

    Updated version dependencies

--- terraform.tf
+++ terraform.tf
@@ -2,29 +2,29 @@ terraform {
   required_version = ">=1.7.0"
 
   required_providers {
-    hcloud = {
-      source  = "hetznercloud/hcloud"
-      version = "1.48.1"
-    }
-
     talos = {
       source  = "siderolabs/talos"
       version = "0.6.1"
     }
 
-    http = {
-      source  = "hashicorp/http"
-      version = ">=3.4.2"
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "1.48.1"
     }
 
     helm = {
       source  = "hashicorp/helm"
-      version = ">=2.12.1"
+      version = "~>2.16.0"
+    }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "~>3.4.0"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = ">=4.0.5"
+      version = "~>4.0.0"
     }
   }
 }
