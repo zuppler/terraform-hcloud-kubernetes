72cea42e1a1ede6927843de31b814cf2cc931f5c
M4t7e <7535523+M4t7e@users.noreply.github.com>
2024-12-19 22:45:51 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -772,7 +772,7 @@ variable "kube_api_extra_args" {
 # Talos CCM
 variable "talos_ccm_version" {
   type        = string
-  default     = "v1.8.1"
+  default     = "v1.8.1" # https://github.com/siderolabs/talos-cloud-controller-manager
   description = "Specifies the version of the Talos Cloud Controller Manager (CCM) to use. This version controls cloud-specific integration features in the Talos operating system."
 }
 
